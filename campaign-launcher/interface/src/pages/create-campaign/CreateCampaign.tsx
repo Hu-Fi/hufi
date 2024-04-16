9b760468ea2b0fe42849c7ada514c1f4adafe0d8
Faouzijedidi1 <faouzijedidi1@gmail.com>
2024-04-16 17:39:36 +0400

    Remove staking restriction

--- campaign-launcher/interface/src/pages/create-campaign/CreateCampaign.tsx
+++ campaign-launcher/interface/src/pages/create-campaign/CreateCampaign.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react';
 
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 import { CampaignForm, CampaignFormValues } from './CampaignForm';
 import { useUploadManifest } from '../../api/manifest';
-import { useCreateEscrow, useLeader, useNotification } from '../../hooks';
+import { useCreateEscrow, useLeader } from '../../hooks';
 import { Loading } from '../../components/loading';
-import { PATHS } from '../../routes';
+// import { PATHS } from '../../routes';
 
 export const CreateCampaign: FC = () => {
   const { mutateAsync: uploadManifest } = useUploadManifest();
   const createEscrow = useCreateEscrow();
   const { loading, leader } = useLeader();
-  const { setNotification } = useNotification();
+  // const { setNotification } = useNotification();
 
   const handleSubmit = async (data: CampaignFormValues) => {
     const { data: manifest } = await uploadManifest({
@@ -23,17 +23,17 @@ export const CreateCampaign: FC = () => {
     await createEscrow(manifest, data.fundAmount);
   };
 
-  if (
-    !loading &&
-    (leader === null || leader?.amountStaked.toString() === '0')
-  ) {
-    setNotification({
-      type: 'warning',
-      message: 'You must stake HMT to create a campaign',
-    });
+  // if (
+  //   !loading &&
+  //   (leader === null || leader?.amountStaked.toString() === '0')
+  // ) {
+  //   setNotification({
+  //     type: 'warning',
+  //     message: 'You must stake HMT to create a campaign',
+  //   });
 
-    return <Navigate to={PATHS.STAKE_HMT} />;
-  }
+  //   return <Navigate to={PATHS.STAKE_HMT} />;
+  // }
 
   return (
     <>{loading ? <Loading /> : <CampaignForm onSubmit={handleSubmit} />}</>
