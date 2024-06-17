0e2267ab272bb7b54c981888e12be7f6b1a0d7de
Eric Lee <eric@hmt.ai>
2024-06-17 13:13:41 -0400

    fix lint

--- campaign-launcher/interface/src/components/crypto-entity/CryptoEntity.tsx
+++ campaign-launcher/interface/src/components/crypto-entity/CryptoEntity.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 
 import { Box, Typography } from '@mui/material';
+import { Address } from 'viem';
 
 import { EXCHANGES, TOKENS } from '../../constants';
 import { ExchangeName, TokenName } from '../../types';
 
 export type CryptoEntityProps = {
-  name: ExchangeName | TokenName;
+  name: ExchangeName | TokenName | Address;
 };
 
 export const CryptoEntity: FC<CryptoEntityProps> = ({ name }) => {
