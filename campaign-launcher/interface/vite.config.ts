fed44572d6a0fd6c596aa3c9fb59f75c328e8493
Ubuntu <ubuntu@ip-172-31-47-64.eu-north-1.compute.internal>
2025-04-03 14:08:59 +0000

    fix

--- campaign-launcher/interface/vite.config.ts
+++ campaign-launcher/interface/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(() => {
     ],
     build: {
       outDir: 'dist',
-      sourcemap: true,
+      sourcemap: false,
       commonjsOptions: {
         transformMixedEsModules: true,
       },
