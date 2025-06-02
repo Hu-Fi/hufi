637459e73a1d9bc6fbd1e88a7c1e49c3c751e424
Dmitry Nechay <lelekkofe@gmail.com>
2025-06-02 15:10:32 +0300

    feat: initial migration w/ setup (#222)

---
+++ recording-oracle/src/modules/auth/token.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { DATABASE_SCHEMA_NAME } from '../../common/constants';
+import type { UserEntity } from '../users';
+
+@Entity({ schema: DATABASE_SCHEMA_NAME, name: 'refresh_tokens' })
+@Index(['userId'], { unique: true })
+export class RefreshTokenEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne('UserEntity', { persistence: false, onDelete: 'CASCADE' })
+  user?: UserEntity;
+
+  @Column()
+  userId: string;
+
+  @Column({ type: 'timestamptz' })
+  expiresAt: Date;
+}
