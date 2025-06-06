e25aa99787b5e9a80e113875364ce6b5bb578a58
Dmitry Nechay <lelekkofe@gmail.com>
2025-06-06 19:28:20 +0300

    chore: change ts config to import ccxt types

--- recording-oracle/types/ccxt.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'ccxt' {
-  const ccxt: {
-    exchanges: string[];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [x: string]: any;
-  };
-  export default ccxt;
-}
