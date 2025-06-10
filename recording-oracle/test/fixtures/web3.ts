d8b4aa78b0a59031d9f9c35f40d937c76bf05735
Dmitry Nechay <lelekkofe@gmail.com>
2025-06-10 19:32:23 +0300

    feat: setup utils tests

---
+++ recording-oracle/test/fixtures/web3.ts
@@ -0,0 +1,29 @@
+import { ethers, Wallet } from 'ethers';
+
+export function generateEthWallet(privateKey?: string) {
+  const wallet = privateKey
+    ? new Wallet(privateKey)
+    : ethers.Wallet.createRandom();
+
+  return {
+    privateKey: wallet.privateKey,
+    address: wallet.address,
+  };
+}
+
+export type SignerMock = jest.Mocked<Pick<Wallet, 'sendTransaction'>> & {
+  __transactionResponse: {
+    wait: jest.Mock;
+  };
+};
+
+export function createSignerMock(): SignerMock {
+  const transactionResponse = {
+    wait: jest.fn(),
+  };
+
+  return {
+    sendTransaction: jest.fn().mockResolvedValue(transactionResponse),
+    __transactionResponse: transactionResponse,
+  };
+}
