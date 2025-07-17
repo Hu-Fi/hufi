83b43ce7d89a681da82db240c258781b5110e87c
Dmitry Nechay <lelekkofe@gmail.com>
2025-07-17 13:17:39 +0300

    feat: use github envs for action secrets

--- reputation-oracle/src/config/env-schema.ts
+++ reputation-oracle/src/config/env-schema.ts
@@ -3,9 +3,20 @@ import * as Joi from 'joi';
 export const envValidator = Joi.object({
   // Web3
   WEB3_PRIVATE_KEY: Joi.string().required(),
-  RPC_URL_SEPOLIA: Joi.string().uri({ scheme: ['http', 'https'] }),
-  RPC_URL_POLYGON: Joi.string().uri({ scheme: ['http', 'https'] }),
-  RPC_URL_POLYGON_AMOY: Joi.string().uri({ scheme: ['http', 'https'] }),
+  /**
+   * Allow empty strings for URLs to pass validation,
+   * because for the code it's the same case
+   * as absence of value, but more conveninent for CI/CD.
+   */
+  RPC_URL_SEPOLIA: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
+  RPC_URL_POLYGON: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
+  RPC_URL_POLYGON_AMOY: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
   RPC_URL_LOCALHOST: Joi.string(),
   // S3
   S3_ENDPOINT: Joi.string(),
