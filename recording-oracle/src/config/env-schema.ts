6f3424dd8e1b0affbbb09ae96f255b4bc90a77e1
Siarhei <sergey.dzeranov@gmail.com>
2025-07-22 15:18:41 +0300

    [V2] Added aurora testnet to the list of supported chains (#280)

--- recording-oracle/src/config/env-schema.ts
+++ recording-oracle/src/config/env-schema.ts
@@ -33,9 +33,23 @@ export const envValidator = Joi.object({
   USE_EXCHANGE_SANDBOX: Joi.string().valid('true', 'false'),
   // Web3
   WEB3_PRIVATE_KEY: Joi.string().required(),
-  RPC_URL_SEPOLIA: Joi.string().uri({ scheme: ['http', 'https'] }),
-  RPC_URL_POLYGON: Joi.string().uri({ scheme: ['http', 'https'] }),
-  RPC_URL_POLYGON_AMOY: Joi.string().uri({ scheme: ['http', 'https'] }),
+  /**
+   * Allow empty strings for URLs to pass validation,
+   * because for the code it's the same case
+   * as absence of value, but more conveninent for CI/CD.
+   */
+  RPC_URL_SEPOLIA: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
+  RPC_URL_POLYGON: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
+  RPC_URL_POLYGON_AMOY: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
+  RPC_URL_AURORA_TESTNET: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .allow(''),
   RPC_URL_LOCALHOST: Joi.string(),
   // S3
   S3_ENDPOINT: Joi.string(),
