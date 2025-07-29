d44dbb1aae04f0981034514150977e731912ddb3
Dzeranov <sergey.dzeranov@gmail.com>
2025-07-29 14:06:54 +0300

    chore: remove s3 leftovers

--- campaign-launcher/server/src/config/config.module.ts
+++ campaign-launcher/server/src/config/config.module.ts
@@ -1,14 +1,13 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { S3ConfigService } from './s3-config-service';
 import { ServerConfigService } from './server-config.service';
 import { Web3ConfigService } from './web3-config.service';
 
 @Global()
 @Module({
   imports: [ConfigModule],
-  providers: [S3ConfigService, ServerConfigService, Web3ConfigService],
-  exports: [S3ConfigService, ServerConfigService, Web3ConfigService],
+  providers: [ServerConfigService, Web3ConfigService],
+  exports: [ServerConfigService, Web3ConfigService],
 })
 export class EnvConfigModule {}
