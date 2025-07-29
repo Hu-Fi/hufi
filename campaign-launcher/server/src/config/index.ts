ea98f6cfb8064ce7b677883230bbe4f9e978d750
Siarhei <sergey.dzeranov@gmail.com>
2025-07-29 14:10:40 +0300

    [V2] Remove s3 leftovers from campaign launcher server (#295)

--- campaign-launcher/server/src/config/index.ts
+++ campaign-launcher/server/src/config/index.ts
@@ -1,6 +1,5 @@
 export * from './env-schema';
 
-export { S3ConfigService } from './s3-config-service';
 export { ServerConfigService } from './server-config.service';
 export { Web3ConfigService } from './web3-config.service';
 
