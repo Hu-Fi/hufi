944c995650e48b8d41b6e4ae83c0e6581b4ca389
Siarhei <sergey.dzeranov@gmail.com>
2025-07-29 14:10:40 +0300

    [V2] Remove s3 leftovers from campaign launcher server (#295)

--- campaign-launcher/server/src/config/env-schema.ts
+++ campaign-launcher/server/src/config/env-schema.ts
@@ -28,11 +28,4 @@ export const envValidator = Joi.object({
 
   RECORDING_ORACLE: Joi.string().pattern(EVM_ADDRESS_REGEX).required(),
   REPUTATION_ORACLE: Joi.string().pattern(EVM_ADDRESS_REGEX).required(),
-  // S3
-  S3_ENDPOINT: Joi.string(),
-  S3_PORT: Joi.number().integer(),
-  S3_ACCESS_KEY: Joi.string().required(),
-  S3_SECRET_KEY: Joi.string().required(),
-  S3_BUCKET: Joi.string(),
-  S3_USE_SSL: Joi.string().valid('true', 'false'),
 });
