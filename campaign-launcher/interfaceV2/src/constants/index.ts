dfe4f99fbeac2b35deccd6f64b21e93d8ad4f9d6
KirillKirill <kirill@hmt.ai>
2025-08-13 20:46:32 +0300

    [Launcher UI V2] Remove fee envs (#314)

--- campaign-launcher/interfaceV2/src/constants/index.ts
+++ campaign-launcher/interfaceV2/src/constants/index.ts
@@ -8,11 +8,8 @@ export const ROUTES = {
 
 export const oracles = {
   exchangeOracle: import.meta.env.VITE_APP_EXCHANGE_ORACLE_ADDRESS,
-  exchangeOracleFee: BigInt(import.meta.env.VITE_APP_EXCHANGE_ORACLE_FEE),
   recordingOracle: import.meta.env.VITE_APP_RECORDING_ORACLE_ADDRESS,
-  recordingOracleFee: BigInt(import.meta.env.VITE_APP_RECORDING_ORACLE_FEE),
   reputationOracle: import.meta.env.VITE_APP_REPUTATION_ORACLE_ADDRESS,
-  reputationOracleFee: BigInt(import.meta.env.VITE_APP_REPUTATION_ORACLE_FEE),
 };
 
 // TODO: Read USDT contract address from Human Protocol SDK
