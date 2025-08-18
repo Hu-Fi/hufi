37f3ebd8a31a8d6ecc7e10de1d2c4aee2a1b6da7
Dmitry Nechay <lelekkofe@gmail.com>
2025-08-18 18:47:34 +0300

    [v2] feat: add version to logs + ability to configure log level (#323)

--- reputation-oracle/src/main.ts
+++ reputation-oracle/src/main.ts
@@ -3,12 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { PayoutsService } from '@/modules/payouts';
 
 import { AppModule } from './app.module';
-import { nestLoggerOverride } from './logger';
+import logger, { nestLoggerOverride } from './logger';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule, {
     logger: nestLoggerOverride,
   });
+
+  logger.debug('Application bootstrapped');
 
   const payoutsService = app.get(PayoutsService);
 
