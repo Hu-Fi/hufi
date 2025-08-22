7aa9d701eb48d66feef5cf783ded721a92144ac0
KirillKirill <kirill@hmt.ai>
2025-08-22 17:23:58 +0300

    [Launcher UI V2] Add Daily Volume Target (#331)

--- campaign-launcher/interfaceV2/src/components/CampaignStats/index.tsx
+++ campaign-launcher/interfaceV2/src/components/CampaignStats/index.tsx
@@ -25,13 +25,25 @@ const StatsCard = styled(Box)(({ theme }) => ({
   border: '1px solid rgba(255, 255, 255, 0.1)',
 
   [theme.breakpoints.down('xl')]: {
-    height: '125px',
+    height: 'unset',
+    minHeight: '125px',
+    justifyContent: 'space-between',
     padding: '16px 32px 24px',
   },
 
   [theme.breakpoints.only('md')]: {
-    height: '125px',
+    height: 'unset',
+    minHeight: '125px',
     padding: '12px',
+  },
+}));
+
+const Title = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  marginBottom: '56px',
+
+  [theme.breakpoints.down('xl')]: {
+    marginBottom: '16px',
   },
 }));
 
@@ -41,10 +53,14 @@ const Value = styled(Typography)(({ theme }) => ({
   fontWeight: 800,
   letterSpacing: '-0.5px',
   lineHeight: '100%',
+}));
 
-  '& > span': {
-    fontSize: '30px',
-  },
+const Suffix = styled('span')(({ theme }) => ({
+  color: theme.palette.primary.violet,
+  fontSize: '30px',
+  fontWeight: 800,
+  letterSpacing: '-0.5px',
+  lineHeight: '100%',
 }));
 
 const FlexGrid = styled(Box)(({ theme }) => ({
@@ -77,51 +93,55 @@ const CampaignStats: FC<Props> = ({ campaign }) => {
 
   const totalFee = campaign.exchange_oracle_fee_percent + campaign.recording_oracle_fee_percent + campaign.reputation_oracle_fee_percent;
   const formattedTokenAmount = +formatTokenAmount(campaign.fund_amount, campaign.fund_token_decimals);
+  const formattedAmountPaid = +formatTokenAmount(campaign.amount_paid, campaign.fund_token_decimals);
 
   return (
     <Grid container spacing={2} width="100%">
       <Grid size={{ xs: 12, md: 6 }}>
         <FlexGrid>
           <StatsCard>
-            <Typography variant="subtitle2" mb={{ xs: 2, xl: 7 }}>Total Funded Amount</Typography>
+            <Title variant="subtitle2">Total Funded Amount</Title>
             <Value>
-              {formattedTokenAmount} <span>{campaign.fund_token_symbol}</span>
+              {formattedTokenAmount}
+              {' '}<Suffix>{campaign.fund_token_symbol}</Suffix>
             </Value>
           </StatsCard>
           <StatsCard>
-            <Typography variant="subtitle2" mb={{ xs: 2, xl: 7 }}>Amount Paid</Typography>
+            <Title variant="subtitle2">Amount Paid</Title>
             <Value>
-              {formatTokenAmount(campaign.amount_paid, campaign.fund_token_decimals)} <span>{campaign.fund_token_symbol}</span>
+              {formattedAmountPaid}
+              {' '}<Suffix>{campaign.fund_token_symbol}</Suffix>
             </Value>
           </StatsCard>
           <StatsCard>
-            <Typography variant="subtitle2" mb={{ xs: 2, xl: 7 }}>Exchange</Typography>
+            <Title variant="subtitle2">Oracle fees</Title>
+            <Value>
+              <FormattedNumber value={formattedTokenAmount * totalFee / 100} /> 
+              {' '}<Suffix>{campaign.fund_token_symbol}</Suffix>{' '}
+              <Typography variant="h6" fontWeight={700} component="span" color="rgba(255, 255, 255, 0.18)">
+                ({totalFee}%)
+              </Typography>
+            </Value>
+          </StatsCard>
+          <StatsCard>
+            <Title variant="subtitle2">Daily volume target</Title>
+            <Typography variant="h5" color="primary.violet" fontWeight={700}>
+              <FormattedNumber value={campaign.daily_volume_target} decimals={3} />
+              {' '}<span>{campaign.trading_pair.split('/')[1]}</span>
+            </Typography>
+          </StatsCard>
+          <StatsCard>
+            <Title variant="subtitle2">Exchange</Title>
             <Typography variant={isXl ? 'h4' : 'h6-mobile'}>
               {exchangeName}
             </Typography>
           </StatsCard>
           <StatsCard>
-            <Typography variant="subtitle2" mb={{ xs: 2, xl: 7 }}>Pair</Typography>
+            <Title variant="subtitle2">Pair</Title>
             <CryptoPairEntity
               symbol={campaign.trading_pair}
               size={isXl ? 'large' : 'medium'}
             />
-          </StatsCard>
-          <StatsCard 
-            sx={{ 
-              flexDirection: { xs: 'column', md: 'row' }, 
-              gap: 2,
-              alignItems: { xs: 'flex-start', md: 'center' }, 
-              py: { xs: 1.5, md: 2 }, 
-              height: { xs: 'unset' }, 
-              flexBasis: '100%'
-            }}
-          >
-            <Typography variant="subtitle2">Oracle fees</Typography>
-            <Typography variant="h6" color="primary.violet" fontWeight={700}>
-              <FormattedNumber value={formattedTokenAmount * totalFee / 100} suffix={' ' + campaign.fund_token_symbol} />
-              ({totalFee}%)
-            </Typography>
           </StatsCard>
         </FlexGrid>
       </Grid>
