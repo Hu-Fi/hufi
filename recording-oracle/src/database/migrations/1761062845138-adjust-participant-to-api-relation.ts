ca7c12d4f32687c44588b74c7042db8f10e4c7e1
Dmitry Nechay <lelekkofe@gmail.com>
2025-10-21 19:14:49 +0300

    wip: user campaigns table migration

---
+++ recording-oracle/src/database/migrations/1761062845138-adjust-participant-to-api-relation.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AdjustParticipantToApiRelation1761062845138
+  implements MigrationInterface
+{
+  name = 'AdjustParticipantToApiRelation1761062845138';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "hu_fi"."user_campaigns" DROP CONSTRAINT "FK_9a1811aa669af54d7809c049525"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "hu_fi"."user_campaigns" DROP COLUMN "exchange_api_key_id"`,
+    );
+  }
+
+  public async down(): Promise<void> {
+    // one-way migraiton
+  }
+}
