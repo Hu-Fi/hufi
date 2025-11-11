de103d40a59297dba8b5e970d27a0763188943f6
portuu3 <adrian.portugues.mas@gmail.com>
2025-11-11 12:38:44 +0100

    remove support

--- campaign-launcher/client/src/App.tsx
+++ campaign-launcher/client/src/App.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Layout from '@/components/Layout';
 import ProtectedRoute from '@/components/ProtectedRoute';
@@ -10,7 +10,6 @@ import { ROUTES } from '@/constants';
 import Campaign from '@/pages/Campaign';
 import Dashboard from '@/pages/Dashboard';
 import ManageApiKeysPage from '@/pages/ManageApiKeys';
-import Support from '@/pages/Support';
 import ActiveAccountProvider from '@/providers/ActiveAccountProvider';
 import ExchangesProvider from '@/providers/ExchangesProvider';
 import { NetworkProvider } from '@/providers/NetworkProvider';
@@ -54,10 +53,6 @@ const App: FC = () => {
                                     <ManageApiKeysPage />
                                   </ProtectedRoute>
                                 }
-                              />
-                              <Route
-                                path={ROUTES.SUPPORT}
-                                element={<Support />}
                               />
                             </Routes>
                           </Layout>
