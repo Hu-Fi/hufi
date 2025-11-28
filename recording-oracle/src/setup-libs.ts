266dbf5f44cba6d61bb12d7d9f16909f4c4607c9
Dmitry Nechay <lelekkofe@gmail.com>
2025-11-28 14:24:23 +0300

    chore: adjust toExpPos

--- recording-oracle/src/setup-libs.ts
+++ recording-oracle/src/setup-libs.ts
@@ -3,10 +3,13 @@ import duration from 'dayjs/plugin/duration';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import Decimal from 'decimal.js';
 
-// Max EVM token decimals is uint8 - 255;
+/**
+ * Max EVM token decimals is uint8: 255
+ * Max ERC-20 value is uint256: 2^256 - 1 (~ 1.158 * 10^77)
+ */
 Decimal.set({
   toExpNeg: -256,
-  toExpPos: 256,
+  toExpPos: 60,
 });
 
 dayjs.extend(duration);
