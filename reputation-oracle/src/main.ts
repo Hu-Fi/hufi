bf155c172c6172dfc89dc16e32e2b48e3145a11f
Dmitry Nechay <lelekkofe@gmail.com>
2025-11-28 13:31:59 +0300

    fix: exp notation decimals

--- reputation-oracle/src/main.ts
+++ reputation-oracle/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import Decimal from 'decimal.js';
 
 import { PayoutsService } from '@/modules/payouts';
 
 import { AppModule } from './app.module';
 import logger, { nestLoggerOverride } from './logger';
 
+function configureLibraries(): void {
+  // Max EVM token decimals is uint8 - 255;
+  Decimal.set({
+    toExpNeg: -256,
+    toExpPos: 256,
+  });
+}
+
 async function bootstrap() {
+  configureLibraries();
+
   const app = await NestFactory.createApplicationContext(AppModule, {
     logger: nestLoggerOverride,
   });
