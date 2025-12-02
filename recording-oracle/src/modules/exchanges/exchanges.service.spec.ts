1035ba5f5d1256b9c94aab82988993650a3c956a
Dmitry Nechay <lelekkofe@gmail.com>
2025-12-02 23:21:13 +0300

    test: exchanges service

---
+++ recording-oracle/src/modules/exchanges/exchanges.service.spec.ts
@@ -0,0 +1,142 @@
+import { faker } from '@faker-js/faker';
+import { createMock } from '@golevelup/ts-jest';
+import { Test } from '@nestjs/testing';
+
+import {
+  ExchangeApiClientFactory,
+  ExchangeApiClient,
+  ExchangePermission,
+} from './api-client';
+import {
+  ExchangeApiKeysService,
+  KeyAuthorizationError,
+} from './exchange-api-keys';
+import { generateExchangeApiKeysData } from './exchange-api-keys/fixtures';
+import { ExchangesService } from './exchanges.service';
+
+const mockExchangeApiClientFactory = createMock<ExchangeApiClientFactory>();
+const mockExchangeApiKeysService = createMock<ExchangeApiKeysService>();
+
+describe('ExchangesService', () => {
+  let exchangesService: ExchangesService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        ExchangesService,
+        {
+          provide: ExchangeApiClientFactory,
+          useValue: mockExchangeApiClientFactory,
+        },
+        {
+          provide: ExchangeApiKeysService,
+          useValue: mockExchangeApiKeysService,
+        },
+      ],
+    }).compile();
+
+    exchangesService = moduleRef.get<ExchangesService>(ExchangesService);
+  });
+
+  it('should be defined', () => {
+    expect(exchangesService).toBeDefined();
+  });
+
+  describe('getClientForUser', () => {
+    const { userId, exchangeName, apiKey, secretKey, extras } =
+      generateExchangeApiKeysData();
+
+    it('should get client via factory with correct params', async () => {
+      mockExchangeApiKeysService.retrieve.mockResolvedValueOnce({
+        id: faker.string.uuid(),
+        apiKey,
+        secretKey,
+        extras,
+      });
+      const mockClient = {} as ExchangeApiClient;
+      mockExchangeApiClientFactory.create.mockReturnValue(mockClient);
+
+      const client = await exchangesService.getClientForUser(
+        userId,
+        exchangeName,
+      );
+
+      expect(client).toBe(mockClient);
+
+      expect(mockExchangeApiClientFactory.create).toHaveBeenCalledTimes(1);
+      expect(mockExchangeApiClientFactory.create).toHaveBeenCalledWith(
+        exchangeName,
+        {
+          apiKey,
+          secret: secretKey,
+          extras,
+          userId,
+        },
+      );
+    });
+  });
+
+  describe('assertUserHasAuthorizedKeys', () => {
+    const { userId, exchangeName } = generateExchangeApiKeysData();
+    const permissionsToCheck = faker.helpers.arrayElements(
+      Object.values(ExchangePermission),
+    );
+
+    const mockExchangeApiClient = createMock<ExchangeApiClient>();
+    let spyOnGetClientForUser: jest.SpyInstance;
+
+    beforeAll(() => {
+      spyOnGetClientForUser = jest.spyOn(exchangesService, 'getClientForUser');
+      spyOnGetClientForUser.mockImplementation(() => mockExchangeApiClient);
+    });
+
+    afterAll(() => {
+      spyOnGetClientForUser.mockRestore();
+    });
+
+    it('should not throw if access check succeeded', async () => {
+      mockExchangeApiClient.checkRequiredAccess.mockResolvedValueOnce({
+        success: true,
+      });
+
+      await exchangesService.assertUserHasAuthorizedKeys(
+        userId,
+        exchangeName,
+        permissionsToCheck,
+      );
+
+      expect(spyOnGetClientForUser).toHaveBeenCalledTimes(1);
+      expect(spyOnGetClientForUser).toHaveBeenCalledWith(userId, exchangeName);
+
+      expect(mockExchangeApiClient.checkRequiredAccess).toHaveBeenCalledTimes(
+        1,
+      );
+      expect(mockExchangeApiClient.checkRequiredAccess).toHaveBeenCalledWith(
+        permissionsToCheck,
+      );
+    });
+
+    it('should throw KeyAuthorizationError if access check fails', async () => {
+      const missingPermission = faker.helpers.arrayElement(permissionsToCheck);
+      mockExchangeApiClient.checkRequiredAccess.mockResolvedValueOnce({
+        success: false,
+        missing: [missingPermission],
+      });
+
+      let thrownError;
+      try {
+        await exchangesService.assertUserHasAuthorizedKeys(
+          userId,
+          exchangeName,
+          permissionsToCheck,
+        );
+      } catch (error) {
+        thrownError = error;
+      }
+
+      expect(thrownError).toBeInstanceOf(KeyAuthorizationError);
+      expect(thrownError.exchangeName).toBe(exchangeName);
+      expect(thrownError.missingPermissions).toEqual([missingPermission]);
+    });
+  });
+});
