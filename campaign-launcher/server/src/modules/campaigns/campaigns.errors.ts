30f1a2931868e50d3997e1d128d2b893e2a2ebf2
Dmitry Nechay <lelekkofe@gmail.com>
2025-12-10 19:28:27 +0300

    feat: add results cutoff backfill

--- campaign-launcher/server/src/modules/campaigns/campaigns.errors.ts
+++ campaign-launcher/server/src/modules/campaigns/campaigns.errors.ts
@@ -19,9 +19,3 @@ export class InvalidCampaignManifestError extends CampaignError {
     super(chainId, address, 'Campaign manifest is not valid');
   }
 }
-
-export class CampaignEscrowNotFoundError extends CampaignError {
-  constructor(chainId: number, address: string) {
-    super(chainId, address, 'Campaign escrow not found');
-  }
-}
