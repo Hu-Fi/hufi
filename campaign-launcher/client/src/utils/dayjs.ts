96a9e359eb3507c89ef8e8ace861994dd002807a
KirillKirill <kirill@hmt.ai>
2026-01-06 18:18:21 +0300

    fix: week starts on monday

--- campaign-launcher/client/src/utils/dayjs.ts
+++ campaign-launcher/client/src/utils/dayjs.ts
@@ -2,9 +2,15 @@ import dayjs from 'dayjs';
 import dayjsDuration from 'dayjs/plugin/duration';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 dayjs.extend(dayjsDuration);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale('en', {
+  weekStart: 1,
+});
 
 export default dayjs;
