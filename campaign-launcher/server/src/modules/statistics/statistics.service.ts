b67b1449c778e84fda04c0969559856d220de780
Dmitry Nechay <lelekkofe@gmail.com>
2026-01-16 15:47:39 +0300

    feat: use RecO for finished campaigns stat

--- campaign-launcher/server/src/modules/statistics/statistics.service.ts
+++ campaign-launcher/server/src/modules/statistics/statistics.service.ts
@@ -1,4 +1,10 @@
-import { OrderDirection, TransactionUtils } from '@human-protocol/sdk';
+import {
+  InvalidKeyError,
+  KVStoreKeys,
+  KVStoreUtils,
+  OrderDirection,
+  TransactionUtils,
+} from '@human-protocol/sdk';
 import { Injectable } from '@nestjs/common';
 import { Interval as ScheduleInterval } from '@nestjs/schedule';
 import dayjs from 'dayjs';
@@ -7,6 +13,7 @@ import { ethers } from 'ethers';
 import { ChainId } from '@/common/constants';
 import { PreventCallOverlap } from '@/common/decorators';
 import Environment from '@/common/utils/environment';
+import * as httpUtils from '@/common/utils/http';
 import { Web3ConfigService } from '@/config';
 import logger from '@/logger';
 import { CampaignsService, CampaignStatus } from '@/modules/campaigns';
@@ -30,23 +37,23 @@ export class StatisticsService {
   async getCampaignsStats(chainId: ChainId): Promise<{
     nActive: number;
     rewardsPoolUsd: number;
-    nCompleted: number | null;
+    nFinished: number | null;
     paidRewardsUsd: number | null;
   }> {
     const [
       { nActive, rewardsPoolUsd },
-      completedCampaignsStats,
+      finishedCampaignsStats,
       totalRewardsStats,
     ] = await Promise.all([
       this.getActiveCampaignsStats(chainId),
-      this.statisticsCache.getCompletedCampaignsStats(chainId),
+      this.getFinishedCampaignsStats(chainId),
       this.statisticsCache.getTotalRewardsStats(chainId),
     ]);
 
     return {
       nActive,
       rewardsPoolUsd,
-      nCompleted: completedCampaignsStats?.nCompleted ?? null,
+      nFinished: finishedCampaignsStats?.nFinished ?? null,
       paidRewardsUsd: totalRewardsStats?.paidRewardsUsd ?? null,
     };
   }
@@ -125,73 +132,89 @@ export class StatisticsService {
     }
   }
 
-  @ScheduleInterval(dayjs.duration(4, 'minutes').asMilliseconds())
-  @PreventCallOverlap()
-  private async refreshCompletedCampaignsStats(): Promise<void> {
-    for (const chainId of this.web3Service.supportedChainIds) {
-      await this.refreshCompletedCampaignsStatsForChain(chainId);
-    }
-  }
-
-  /**
-   * Refreshes in-memory stats on number of completed campaigns for chain.
-   *
-   * Takes into account both "completed" and "cancelled" because
-   * cancellation might have been requested in mid-campaign
-   * and technically our platform successfully operated on them.
-   */
-  private async refreshCompletedCampaignsStatsForChain(
+  private async getFinishedCampaignsStats(
     chainId: ChainId,
-  ): Promise<void> {
+  ): Promise<{ nFinished: number } | null> {
     try {
-      this.logger.debug('Refreshing completed campaigns stats', {
-        chainId,
-      });
+      let finishedCampaignsStats =
+        await this.statisticsCache.getFinishedCampaignsStats(chainId);
 
-      /**
-       * Currently SDK doesn't allow necessary stats queries,
-       * so we have to recalculate N of completed campaigns on every run.
-       *
-       * It's necessary for correct stat calculation, because there might be next case:
-       * - campaign C1 createdAt C1_T1, completed at C1_T2
-       * - campaign C2 createdAt C2_T1, completed at C2_T2
-       * - C2_T1 > C1_T1, C2_T2 < C1_T2
-       * so C2 completes earlier than C1, we remember its createdAt (because it's default
-       * orderBy prop that can't be changed) and never count C1 when it completes.
-       */
-      let nCompleted = 0;
-      do {
-        const campaigns = await this.campaignsService.getCampaigns(
-          chainId,
-          {
-            statuses: [CampaignStatus.COMPLETED, CampaignStatus.CANCELLED],
-          },
-          {
-            limit: 100,
-            skip: nCompleted,
-          },
-        );
-
-        if (campaigns.length === 0) {
-          break;
+      if (!finishedCampaignsStats) {
+        let recordingOracleUrl: string;
+        try {
+          recordingOracleUrl = await KVStoreUtils.get(
+            chainId as number,
+            this.web3ConfigService.recordingOracle,
+            KVStoreKeys.url,
+          );
+        } catch (error) {
+          if (error instanceof InvalidKeyError) {
+            recordingOracleUrl = '';
+          } else {
+            this.logger.error(
+              'Failed to get Recording Oracle url from KV store',
+              {
+                chainId,
+                error,
+              },
+            );
+            return null;
+          }
         }
 
-        nCompleted += campaigns.length;
-        // eslint-disable-next-line no-constant-condition
-      } while (true);
+        if (!httpUtils.isValidHttpUrl(recordingOracleUrl)) {
+          this.logger.debug('Recording Oracle url is not valid', {
+            chainId,
+            recordingOracleUrl,
+          });
+          return null;
+        } else {
+          recordingOracleUrl = recordingOracleUrl.replace(/\/$/, '');
+        }
 
-      await this.statisticsCache.setCompletedCampaignsStats(chainId, {
-        nCompleted,
-      });
+        let nFinished: number;
+        try {
+          const response = await fetch(`${recordingOracleUrl}/stats/campaigns`);
+          if (!response.ok) {
+            throw new Error(
+              `Error response from Recording Oracle: ${response.status}`,
+            );
+          }
 
-      this.logger.debug('Completed campaigns stats refreshed', {
-        nCompleted,
-      });
+          const responseJson = await response.json();
+          nFinished = responseJson.n_finished;
+        } catch (error) {
+          this.logger.error(
+            'Failed to get campaigns stats from Recording Oracle',
+            {
+              chainId,
+              recordingOracleUrl,
+              error,
+            },
+          );
+
+          return null;
+        }
+
+        finishedCampaignsStats = {
+          nFinished,
+        };
+
+        await this.statisticsCache.setFinishedCampaignsStats(
+          chainId,
+          finishedCampaignsStats,
+        );
+      }
+
+      return finishedCampaignsStats;
     } catch (error) {
-      this.logger.error('Failed to refresh completed campaigns stats', {
+      const errorMessage = 'Failed to get active campaigns stats';
+      this.logger.error(errorMessage, {
         chainId,
         error,
       });
+
+      throw new Error(errorMessage);
     }
   }
 
