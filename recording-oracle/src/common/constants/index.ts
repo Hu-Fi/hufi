ab4a87f0b499111b1b6bbb8fd6fdd6e57e0f3df3
Dmitry Nechay <lelekkofe@gmail.com>
2026-01-21 18:56:05 +0300

    refactor: supported exchanges constants

--- recording-oracle/src/common/constants/index.ts
+++ recording-oracle/src/common/constants/index.ts
@@ -5,22 +5,10 @@ export const DATABASE_SCHEMA_NAME = 'hu_fi';
 export const DEFAULT_NONCE = 'signup';
 export const JWT_STRATEGY_NAME = 'jwt-http';
 
-export const SUPPORTED_EXCHANGE_NAMES = [
-  'bigone',
-  'bitmart',
-  'bybit',
-  'gate',
-  'htx',
-  'kraken',
-  'mexc',
-  'upbit',
-  'xt',
-] as const;
-export type SupportedExchange = (typeof SUPPORTED_EXCHANGE_NAMES)[number];
-
 export const EVM_SIGNATURE_REGEX = /^0x[0-9a-fA-F]{130}$/;
 
 export const DEFAULT_PAGINATION_LIMIT = 10;
 
 export * from './chains';
+export * from './exchanges';
 export * from './token';
