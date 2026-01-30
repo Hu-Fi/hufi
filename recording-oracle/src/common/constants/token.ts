d706e4016c4be9ee574177f0465f7e5434647b5e
Dmitry Nechay <lelekkofe@gmail.com>
2026-01-30 18:00:04 +0300

    feat: remove unused const

--- recording-oracle/src/common/constants/token.ts
+++ recording-oracle/src/common/constants/token.ts
@@ -1,5 +1,3 @@
-import { ChainId as SdkChainId } from '@human-protocol/sdk';
-
 export const ERC20_ABI_DECIMALS = [
   'function decimals() view returns (uint8)',
 ] as const;
@@ -11,7 +9,3 @@ export const ERC20_ABI_SYMBOL = [
 export const ETH_TOKEN_SYMBOL = 'ETH';
 
 export const ETH_USDT_PAIR = 'ETH/USDT';
-
-export const USDT_TOKEN_ADDRESS: Partial<Record<SdkChainId, string>> = {
-  [SdkChainId.BSC_MAINNET]: '0x55d398326f99059fF775485246999027B3197955',
-};
