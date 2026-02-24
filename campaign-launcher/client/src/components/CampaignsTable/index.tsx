5c2cd4d201ccae78fdb76a90d12591f5e2d0d9b6
KirillKirill <kirill@hmt.ai>
2026-02-24 15:56:47 +0300

    [Campaign Launcher UI] Refactor providers around web3 (#760)

--- campaign-launcher/client/src/components/CampaignsTable/index.tsx
+++ campaign-launcher/client/src/components/CampaignsTable/index.tsx
@@ -15,8 +15,8 @@ import {
   useIsLgDesktop,
   useIsMobile,
 } from '@/hooks/useBreakpoints';
-import useRetrieveSigner from '@/hooks/useRetrieveSigner';
 import { useExchangesContext } from '@/providers/ExchangesProvider';
+import { useSignerContext } from '@/providers/SignerProvider';
 import { useWeb3Auth } from '@/providers/Web3AuthProvider';
 import type { Campaign } from '@/types';
 import {
@@ -57,9 +57,9 @@ const formatDate = (dateString: string) => {
 };
 
 const MyCampaignsNoRows: FC = () => {
-  const { signer } = useRetrieveSigner();
+  const { isSignerReady } = useSignerContext();
 
-  if (!signer) {
+  if (!isSignerReady) {
     return (
       <>
         <Typography variant="subtitle2" component="p">
