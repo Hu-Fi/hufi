a4cdf52e6352d6285d64e1f74ab93d9325fd3982
Dmitry Nechay <lelekkofe@gmail.com>
2026-03-10 18:45:33 +0300

    feat: use hufi subgraph

--- campaign-launcher/server/src/common/constants/index.ts
+++ campaign-launcher/server/src/common/constants/index.ts
@@ -5,6 +5,5 @@ export const EVM_ADDRESS_REGEX = /^0x[0-9a-fA-F]{40}$/;
 export const DEFAULT_PAGINATION_LIMIT = 10;
 
 export * from './chains';
-export * from './escrow';
 export * from './exchanges';
 export * from './token';
