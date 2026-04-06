49fdf69d126e2eb09e49ea9d713af3919194d85b
KirillKirill <kirill@hmt.ai>
2026-04-06 11:57:56 +0300

    fix: remove unused imports

--- campaign-launcher/client/src/components/CampaignsTable/index.tsx
+++ campaign-launcher/client/src/components/CampaignsTable/index.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 
-import { Box, Button, IconButton, Typography } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 import { DataGrid, type GridColDef } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router';
 
