4264bd46d4730ee1fe92d04227971e80cbc5a27c
KirillKirill <kirill@hmt.ai>
2026-05-27 12:57:48 +0300

    chore: minor code improvements

--- campaign-launcher/client/src/types/index.ts
+++ campaign-launcher/client/src/types/index.ts
@@ -1,5 +1,11 @@
 import type { ChainId } from '@human-protocol/sdk';
 
+export type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
 export type EvmAddress = `0x${string}`;
 
 export enum ExchangeType {
